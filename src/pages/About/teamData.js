7957c1218aa886e0af4342c3ae0de9bdf5ee861e
AMani1dev <139342694+amine1111111@users.noreply.github.com>
2025-07-19 09:54:21 +0100

    first commit

---
+++ src/pages/About/teamData.js
@@ -0,0 +1,97 @@
+// export let firstArray = [
+//   {
+//     content: "/about/images/squad-1.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-2.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-3.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-4.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-5.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-6.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-7.jpg",
+//   },
+// ];
+
+// export let secondArray = [
+//   {
+//     content: "/about/images/squad-1.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-2.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-3.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-4.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-5.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-6.jpg",
+//   },
+//   {
+//     content: "/about/images/squad-7.jpg",
+//   },
+// ];
+
+const base = import.meta.env.VITE_BASE_URL;
+
+export let firstArray = [
+  {
+    content: `${base}about/images/squad-1.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-2.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-3.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-4.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-5.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-6.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-7.jpg`,
+  },
+];
+
+export let secondArray = [
+  {
+    content: `${base}about/images/squad-1.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-2.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-3.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-4.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-5.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-6.jpg`,
+  },
+  {
+    content: `${base}about/images/squad-7.jpg`,
+  },
+];
